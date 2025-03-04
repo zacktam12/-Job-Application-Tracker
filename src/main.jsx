a17f51d663bb56ea6a2dccd6e0a8f0bf127196f6
hp <zekariastamiru12@gmail.com>
2025-03-04 14:00:43 +0300

    wrap App component with Redux Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
 import React from "react";
-import "./index.css";
+import "../index.css";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
