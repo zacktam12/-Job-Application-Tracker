98981dbb9589a4fce757b8b92576c588c944bf18
hp <zekariastamiru12@gmail.com>
2025-03-04 12:42:54 +0300

    add Tailwind CSS setup and Redux store configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+
 function App() {
-  return <div>hello</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-red-100 text-2xl font-semibold text-gray-800">
+      Hello, Tailwind!
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import React from "react";
-
+import "./index.css";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
