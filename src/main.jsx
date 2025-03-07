168f95dee3050a4e2c519ac102cf4a4cb4bdaa26
hp <zekariastamiru12@gmail.com>
2025-03-07 08:58:27 +0300

    refactor main entry point to use ReactDOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "../index.css";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import store from "./redux/store.js";
 import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import "./styles/global.css";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
