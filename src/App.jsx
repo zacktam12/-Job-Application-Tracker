3f6b4c3cb901d0a0153216a2423fefd38b067bb4
hp <zekariastamiru12@gmail.com>
2025-03-09 22:31:54 +0300

    add routing for Login, Register, JobDetails, AddJob, and NotFound pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import JobDetails from "./pages/JobDetails";
+import AddJob from "./pages/AddJob";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/job/:id" element={<JobDetails />} />
+        <Route path="/add-job" element={<AddJob />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
