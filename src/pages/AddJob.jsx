fc8e8947859c347e8902a6f054c67c993c87a024
hp <zekariastamiru12@gmail.com>
2025-03-10 22:34:56 +0300

    add AddJob component

---
+++ src/pages/AddJob.jsx
@@ -0,0 +1,12 @@
+import JobForm from "../components/JobForm";
+
+const AddJob = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-4">Add New Job</h2>
+      <JobForm />
+    </div>
+  );
+};
+
+export default AddJob;
