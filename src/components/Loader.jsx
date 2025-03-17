2425de3d349ec432a5ac167e1fccd35058d7900b
hp <zekariastamiru12@gmail.com>
2025-03-17 22:39:52 +0300

    add Loader component with corresponding CSS for styling

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Loader.css"; // Make sure to create a corresponding CSS file for styling
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+
+export default Loader;
