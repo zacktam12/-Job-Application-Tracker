e1e4818963ee98c01ff1defb47384a93645e64b5
hp <zekariastamiru12@gmail.com>
2025-03-21 23:40:45 +0300

    add Modal component

---
+++ src/components/Modal.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Modal.css";
+
+const Modal = ({ show, onClose, children }) => {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="modal-backdrop">
+      <div className="modal-content">
+        <button className="modal-close" onClick={onClose}>
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
