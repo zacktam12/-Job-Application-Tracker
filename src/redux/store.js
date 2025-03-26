ff901ff7e06355abeadc2e919e5ae19896c55c2f
hp <zekariastamiru12@gmail.com>
2025-03-26 22:05:26 +0300

    add some clarification

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import jobReducer from "./jobs/jobSlice";
 import authReducer from "./auth/authSlice";
 
+// this is store for redux
 const store = configureStore({
   reducer: {
     jobs: jobReducer,
