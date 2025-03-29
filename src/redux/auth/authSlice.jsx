75dd140c2850e48ae94ae2aca0a906304b8e5b93
hp <zekariastamiru12@gmail.com>
2025-03-29 20:23:05 +0300

    add registerSuccess and updateUserDetails actions to authSlice

--- src/redux/auth/authSlice.jsx
+++ src/redux/auth/authSlice.jsx
@@ -17,8 +17,18 @@ const authSlice = createSlice({
       state.user = null;
       state.isAuthenticated = false;
     },
+    registerSuccess: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+    },
+    updateUserDetails: (state, action) => {
+      if (state.user) {
+        state.user = { ...state.user, ...action.payload };
+      }
+    },
   },
 });
 
-export const { loginSuccess, logout } = authSlice.actions;
+export const { loginSuccess, logout, registerSuccess, updateUserDetails } =
+  authSlice.actions;
 export default authSlice.reducer;
